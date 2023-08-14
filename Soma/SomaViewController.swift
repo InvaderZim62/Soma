81cca36cc0f0ce3dd8f6d80a1f2945f77b6acca2
Invader Zim <pwdelete@gmail.com>
2023-08-14 16:16:35 -0700

    Initial setup.

---
+++ Soma/SomaViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SomaViewController.swift
+//  Soma
+//
+//  Created by Phil Stern on 8/14/23.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class SomaViewController: UIViewController {
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
