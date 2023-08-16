e6e7aea58bbfe1b4a6d23531cb90b84ed43065bd
Invader Zim <pwdelete@gmail.com>
2023-08-16 16:46:32 -0700

    Added tap and swipe gestures to select and rotate shapes.  Note: some combinations of rotations don't work (gimbal lock?).

--- Soma/SomaViewController.swift
+++ Soma/SomaViewController.swift
@@ -28,7 +28,8 @@ class SomaViewController: UIViewController {
     var cameraNode: SCNNode!
     var scnView: SCNView!
 
-    var shapeNodes = [ShapeNode]()
+    var shapeNodes = [ShapeNode]()  // pws: is this needed?
+    var selectedShapeNode: ShapeNode?
     var startingPositions = [SCNVector3]()
 
     override var prefersStatusBarHidden: Bool {
@@ -40,8 +41,29 @@ class SomaViewController: UIViewController {
         setupScene()
         setupCamera()
         setupView()
-        computeStartingPositions()
+        startingPositions = getEvenlySpacedEllipticalPoints(number: ShapeType.count, horizontalRadius: 3.5, verticalRadius: 7.0).shuffled()
         createShapeNodes()
+
+        // add tap gesture to select shapes, and rotate about screen z-axis
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        scnView.addGestureRecognizer(tapGesture)
+        
+        // add swipe gestures to rotate shapes about screen x- and y-axes
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeUp.direction = .up
+        scnView.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeDown.direction = .down
+        scnView.addGestureRecognizer(swipeDown)
+
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeLeft.direction = .left
+        scnView.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeRight.direction = .right
+        scnView.addGestureRecognizer(swipeRight)
     }
 
     private func createShapeNodes() {
@@ -52,6 +74,56 @@ class SomaViewController: UIViewController {
             shapeNode.eulerAngles.x = [0, .pi].randomElement()!  // rotate around x-axis (0 or 180 deg)
             shapeNodes.append(shapeNode)
             scnScene.rootNode.addChildNode(shapeNode)
+        }
+    }
+    
+    // MARK: - Gesture Recognizers
+    
+    // make tapped shape the selected shape, or rotate about scene z-axis, if already selected
+    @objc private func handleTap(recognizer: UITapGestureRecognizer) {
+        let location = recognizer.location(in: scnView)
+        if let tappedShape = getShapeNodeAt(location) {
+            if tappedShape == selectedShapeNode {
+                // selected shape tapped again (rotate 90 degrees about negative z-axis)
+                tappedShape.runAction(SCNAction.rotateBy(x: 0, y: 0, z: -.pi/2, duration: 0.2))
+            } else {
+                // new shape tapped (select it)
+                selectedShapeNode = tappedShape
+            }
+        } else {
+            // no shape tapped (deselect all)
+            selectedShapeNode = nil
+        }
+    }
+    
+    // get shape node at location provided by tap gesture (nil if none tapped)
+    private func getShapeNodeAt(_ location: CGPoint) -> ShapeNode? {
+        var shapeNode: ShapeNode?
+        let hitResults = scnView.hitTest(location, options: nil)  // nil returns closest hit
+        if let result = hitResults.first(where: { $0.node.parent?.name == "Shape Node" }) {
+            shapeNode = result.node.parent as? ShapeNode
+        }
+        return shapeNode
+    }
+    
+    // rotate selected shape 90 degrees at a time (vertical pan about scene x-axis, lateral pan about scene y-axis)
+    @objc func handleSwipe(recognizer: UISwipeGestureRecognizer) {
+        if let selectedShapeNode {
+            var sceneRotation: SCNVector3
+            switch recognizer.direction {
+            case .up:
+                sceneRotation = SCNVector3(-CGFloat.pi/2, 0, 0)
+            case .down:
+                sceneRotation = SCNVector3(CGFloat.pi/2, 0, 0)
+            case .left:
+                sceneRotation = SCNVector3(0, -CGFloat.pi/2, 0)
+            case .right:
+                sceneRotation = SCNVector3(0, CGFloat.pi/2, 0)
+            default:
+                sceneRotation = SCNVector3(0, 0, 0)
+            }
+            let nodeRotation = scnScene.rootNode.convertVector(sceneRotation, to: selectedShapeNode)
+            selectedShapeNode.runAction(SCNAction.rotateBy(x: CGFloat(nodeRotation.x), y: CGFloat(nodeRotation.y), z: CGFloat(nodeRotation.z), duration: 0.2))
         }
     }
 
@@ -71,7 +143,7 @@ class SomaViewController: UIViewController {
 
     private func setupView() {
         scnView = self.view as? SCNView
-        scnView.allowsCameraControl = true  // true: allow standard camera controls with swiping
+        scnView.allowsCameraControl = false  // false: move camera programmatically
         scnView.showsStatistics = true
         scnView.autoenablesDefaultLighting = true
         scnView.isPlaying = true  // prevent SceneKit from entering a "paused" state, if there isn't anything to animate
@@ -80,14 +152,13 @@ class SomaViewController: UIViewController {
     
     // MARK: - Utility functions
 
-    // compute 7 equally-spaced (shuffled) positions around an ellipse
-    private func computeStartingPositions() {
-        let a = 3.5  // horizontal radius
-        let b = 7.0  // vertical radius
+    // compute equally-spaced positions around a 3D ellipse at z = 0
+    private func getEvenlySpacedEllipticalPoints(number: Int, horizontalRadius a: Double, verticalRadius b: Double) -> [SCNVector3] {
+        var points = [SCNVector3]()
         let circumference = 1.85 * Double.pi * sqrt((a * a + b * b) / 2) // reasonable approximation (no exact solution)
-        // Note: will have less than 7 shapes, if circumference is over-estimated
-        let desiredSpacing = circumference / Double(ShapeType.count)
-        let resolution = 100  // check every 100/360 deg for next position with desired spacing
+        // Note: will have less than "number" shapes, if circumference is over-estimated
+        let desiredSpacing = circumference / Double(number)
+        let resolution = 200  // check every 1.8 deg for next position with desired spacing
         var pastX = 10.0
         var pastY = 10.0
         var count = 0
@@ -100,13 +171,13 @@ class SomaViewController: UIViewController {
             let y = radius * sin(theta)
             let spacing = sqrt(pow((x - pastX), 2) + pow(y - pastY, 2))
             if spacing > desiredSpacing {
-                startingPositions.append(SCNVector3(radius * cos(theta), radius * sin(theta), 0))
+                points.append(SCNVector3(radius * cos(theta), radius * sin(theta), 0))
                 count += 1
-                if count == ShapeType.count { break }
+                if count == number { break }
                 pastX = x
                 pastY = y
             }
         }
-        startingPositions.shuffle()
+        return points
     }
 }
